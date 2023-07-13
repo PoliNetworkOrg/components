fb6c5aafcef74d01d6d8b9bcbb41595044e8d86e
toto04 <tommaso.morganti01@gmail.com>
2023-07-13 11:12:57 +0000

    Deploying to gh-pages from @ PoliNetworkOrg/components@2af285ad2bb8575cfb0b9d2fcd0b0d400c824162 🚀

---
+++ assets/Button.stories-02913dc2.js
@@ -0,0 +1,21 @@
+import{B as b}from"./Button-1703f682.js";import"./jsx-runtime-94f6e698.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";const f={title:"Example/Button",component:b,tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}}},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},s={args:{size:"small",label:"Button"}};var o,t,n;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: "Button"
+  }
+}`,...(n=(t=r.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var l,c,m;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    label: "Button"
+  }
+}`,...(m=(c=a.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var p,u,i;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    label: "Button"
+  }
+}`,...(i=(u=e.parameters)==null?void 0:u.docs)==null?void 0:i.source}}};var d,g,B;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    label: "Button"
+  }
+}`,...(B=(g=s.parameters)==null?void 0:g.docs)==null?void 0:B.source}}};const E=["primary","secondary","large","small"];export{E as __namedExportsOrder,f as default,e as large,r as primary,a as secondary,s as small};
+//# sourceMappingURL=Button.stories-02913dc2.js.map
