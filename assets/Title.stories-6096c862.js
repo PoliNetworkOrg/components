1ecfdd95f296376ecaedc83b764ddd5db7ada6ff
toto04 <tommaso.morganti01@gmail.com>
2023-07-26 16:11:53 +0000

    Deploying to gh-pages from @ PoliNetworkOrg/components@e2a24fce8d393ce08eee30929121a684e28fdd20 🚀

--- assets/Title.stories-6096c862.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"./jsx-runtime-94f6e698.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";const m=""+new URL("logo-88c172a4.svg",import.meta.url).href;const a=({section:o,onPress:s})=>e.jsxs("div",{onClick:()=>s==null?void 0:s(),className:"polinetwork-title",children:[e.jsx("img",{draggable:!1,src:m}),e.jsx("span",{style:{fontWeight:700},children:"PoliNetwork"}),o&&e.jsx("span",{style:{fontWeight:500},children:o})]});try{a.displayName="Title",a.__docgenInfo={description:"Main Title for top left corner",displayName:"Title",props:{onPress:{defaultValue:null,description:"Callback for when the user clicks on the title",name:"onPress",required:!1,type:{name:"(() => void)"}},section:{defaultValue:null,description:"Optional section name, lower font weight",name:"section",required:!1,type:{name:"string"}}}}}catch{}const h={title:"PoliNetwork/Title",component:a,tags:["autodocs"],argTypes:{section:{control:"text"}}},t={args:{}},r={args:{section:"Rankings"}};var n,i,l;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
-  args: {}
-}`,...(l=(i=t.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};var c,p,d;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
-  args: {
-    section: "Rankings"
-  }
-}`,...(d=(p=r.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const k=["plain","rankings"];export{k as __namedExportsOrder,h as default,t as plain,r as rankings};
-//# sourceMappingURL=Title.stories-6096c862.js.map
--- assets/preview-62b79e5f.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-7682d07f.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-2e962bb8.js"),["./DocsRenderer-EYKKDMVH-2e962bb8.js","./iframe-7682d07f.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./react-18-f58e807b.js","./index-80ae7d84.js","./index-c5ee637c.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-62b79e5f.js.map
