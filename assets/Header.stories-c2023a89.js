27f1e594ca888e3cb1397b473ca5c924164987f2
toto04 <tommaso.morganti01@gmail.com>
2023-07-26 20:41:26 +0000

    Deploying to gh-pages from @ PoliNetworkOrg/components@5240034ebba582de545c0f70c6adfdadeec52566 🚀

--- assets/Header.stories-c2023a89.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./jsx-runtime-94f6e698.js";import{T as d}from"./Title-ba0b7549.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";const m=""+new URL("theme-1ef2cbaf.svg",import.meta.url).href;const a=s=>t.jsxs("div",{className:"polinetwork-header",children:[t.jsx(d,{onPress:s.onPress,section:s.section}),t.jsx("img",{draggable:!1,src:m})]});try{a.displayName="Header",a.__docgenInfo={description:"Header component for the upper bar with title and theme selector",displayName:"Header",props:{onPress:{defaultValue:null,description:"Callback for when the user clicks on the title",name:"onPress",required:!1,type:{name:"(() => void)"}},section:{defaultValue:null,description:"Optional section name, lower font weight",name:"section",required:!1,type:{name:"string"}}}}}catch{}const _={title:"PoliNetwork/Header",component:a,tags:["autodocs"],parameters:{layout:"fullscreen"},argTypes:{section:{control:"text"}}},e={args:{}},r={args:{section:"Rankings"}};var o,n,c;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {}
-}`,...(c=(n=e.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var i,l,p;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
-  args: {
-    section: "Rankings"
-  }
-}`,...(p=(l=r.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};const k=["plain","rankings"];export{k as __namedExportsOrder,_ as default,e as plain,r as rankings};
-//# sourceMappingURL=Header.stories-c2023a89.js.map
---
+++ assets/Title.stories-036a83dd.js
@@ -0,0 +1,8 @@
+import{T as i}from"./Title-1ce46c58.js";import"./jsx-runtime-94f6e698.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";const l={title:"PoliNetwork/Title",component:i,tags:["autodocs"],argTypes:{section:{control:"text"}}},r={args:{}},s={args:{section:"Rankings"}};var e,a,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {}
+}`,...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};var t,n,c;s.parameters={...s.parameters,docs:{...(t=s.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    section: "Rankings"
+  }
+}`,...(c=(n=s.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const u=["plain","rankings"];export{u as __namedExportsOrder,l as default,r as plain,s as rankings};
+//# sourceMappingURL=Title.stories-036a83dd.js.map
