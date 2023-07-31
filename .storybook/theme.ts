b030904db61d1be6cc82815ffcba2e14ca0519ad
Tommaso Morganti <tommaso.morganti01@gmail.com>
2023-07-31 19:25:31 +0200

    added theming

---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create"
+
+export default create({
+  base: "light",
+  brandTitle: "PoliNetwork",
+  brandUrl: "https://polinetwork.org",
+  brandImage: "./pn.svg",
+  brandTarget: "_self",
+})
